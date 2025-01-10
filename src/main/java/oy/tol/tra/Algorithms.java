a2f6ee172d989fe72671759f3392b88bf39e6624
perakarhis <51946499+perakarhis@users.noreply.github.com>
2025-01-10 13:07:18 +0200

    Update Algorithms.java

--- src/main/java/oy/tol/tra/Algorithms.java
+++ src/main/java/oy/tol/tra/Algorithms.java
@@ -1,47 +1,33 @@
 package oy.tol.tra;
 
-// TEACHERS: TODO: REMOVE this file before giving the material to students!
-// TEACHERS: TODO: Also move the GenericTests.java to root dir of this project.
-// Students are supposed to move the sorting / reversing to this new class and then
-// implement the algorithms as generic to be used also in later exercises.
-
 public class Algorithms {
 
-   private Algorithms() {
-      // Nada
-   }
 
-   // Slow insertion sort is ok here, since it was taught in Programming 2.
-   public static <T extends Comparable<T>> void sort(T[] array) {
-      if (null == array) {
-         return;
-      }
-      int i = array.length - 1;
-      while (i > 0) {
-         for (int index = i - 1; index >= 0; index--) {
-            if (array[i].compareTo(array[index]) < 0) {
-               swap(array, i, index);
-            }
-         }
-         i--;
-      }
-   }
+    public static <T> void reverse(T[] array) {
+        int i = 0;
+        int j = array.length - 1;
+        while (i < j) {
+            swap(array, i, j);
+            i++;
+            j--;
+        }
+    }
 
-   public static <T> void reverse(T[] array) {
-      if (null == array) {
-         return;
-      }
-      int i = 0;
-      while (i < array.length / 2) {
-         swap(array, i, array.length - i - 1);
-         i++;
-      }
-   }
+    public static <T extends Comparable<T>> void sort(T[] array) {
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++)
+            for (int j = 0; j < n - i - 1; j++)
+                if (array[j].compareTo(array[j + 1]) > 0) {
+                    swap(array, j, j + 1);
+                }
+    }
 
-   public static <T> void swap(T[] array, int first, int second) {
-      T temp = array[first];
-      array[first] = array[second];
-      array[second] = temp;
-   }
 
+    //Swaps two elements in an array.
+
+    public static <T> void swap(T[] array, int i, int j) {
+        T temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
 }
